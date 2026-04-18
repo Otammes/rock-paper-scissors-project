6c79b7c2d9e1127783fbe88de7a5239b548273e5
Otammes <269725913+Otammes@users.noreply.github.com>
2026-04-18 16:55:44 +0200

    Add script with getComputerChoice function

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("Hello")
+
+
+
+function getComputerChoice(outcome) {
+    if (outcome <= 0.33)
+        return("rock")
+    else if (outcome >= 0.34 && outcome <= 0.67)
+        return("paper")
+    else
+        return("scissors")
+
+}
+
+let outcome = Math.random();
+console.log(getComputerChoice(outcome))
